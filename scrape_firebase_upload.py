8f0a669374b06d56441eacd100112f59757c5a43
Lokesh T M <156338333+lokesh-t-m@users.noreply.github.com>
2024-05-29 23:49:22 +0530

    scrape_firebase_upload.py

--- scrape_firebase_upload.py
+++ scrape_firebase_upload.py
@@ -1,10 +1,11 @@
-import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import firebase_admin
+from firebase_admin import credentials,db
 
 # Function to log in to HackerRank
 def login(username, password):
@@ -12,19 +13,19 @@ def login(username, password):
         driver = webdriver.Chrome()
         driver.get('https://www.hackerrank.com/auth/login')
         username_field = WebDriverWait(driver, 20).until(
-            EC.visibility_of_element_located((By.NAME,"username"))
+            EC.visibility_of_element_located((By.NAME, "username"))
         )
         username_field.send_keys(username)
         password_field = WebDriverWait(driver, 20).until(
-            EC.visibility_of_element_located((By.NAME,"password"))
+            EC.visibility_of_element_located((By.NAME, "password"))
         )
         password_field.send_keys(password)
-        login_button=WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR,'button[data-hr-focus-item="private"]'))
+        login_button = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-hr-focus-item="private"]'))
         )
         login_button.click()
-
-
+        
+        
         
         WebDriverWait(driver, 20).until(
             EC.url_contains("hackerrank.com/dashboard")
@@ -72,23 +73,45 @@ def scrape_leaderboard(driver, url, seen_usernames):
 
 # Function to scrape usernames and scores from 20 pages max
 def scrape_all_leaderboard(driver):
-    all_user_scores = {}
-    seen_usernames = set()
+    try:
+        all_user_scores = {}
+        seen_usernames = set()
     
-    for page in range(1, 21):
-        url = f'https://www.hackerrank.com/leaderboard?filter=follows&filter_on=friends&page={page}&track=algorithms&type=practice'
-        usernames, scores = scrape_leaderboard(driver, url, seen_usernames)
+        for page in range(1, 21):
+            url = f'https://www.hackerrank.com/leaderboard?filter=follows&filter_on=friends&page={page}&track=algorithms&type=practice'
+            usernames, scores = scrape_leaderboard(driver, url, seen_usernames)
         
-        for i in range(len(usernames)):
-            all_user_scores[usernames[i]] = scores[i]
-        if len(usernames)<2:
-            break
+            for i in range(len(usernames)):
+                all_user_scores[usernames[i]] = scores[i]
+                
+            if len(usernames)<2:
+                break
     
-    return all_user_scores
+        return all_user_scores
+    except Exception as e:
+        print("Failed to scrape the first 20 page data",e)
 
-def save_to_json(data, filename):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
+
+        
+        
+#Function to update data in Firebase        
+def firebase_upload(leaderboard_data):
+    try:
+        credential=credentials.Certificate('credentials.json')
+        firebase_admin.initialize_app(credential,{'databaseURL':'https://certain-root-333610-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+        ref=db.reference('/')
+        ref.set({})
+        
+        rank=1
+        for username,score in leaderboard_data.items():
+            ref.child('Leaderboard').child(str(rank)).set({'Username':username,'score':score})
+            rank+=1
+        print("Firebase database has updated")
+        
+        
+    except Exception as e:
+        print("Failed to upload data to Firebase",e)
+    
 
 # Credentials
 username = ''
@@ -104,6 +127,7 @@ if driver:
         print("Usernames and Scores from the leaderboard:")
         for username, score in sorted_user_scores.items():
             print(f"Username: {username}, Score: {score}")
-        save_to_json(sorted_user_scores, 'leaderboard_data.json')
-        print("Data saved to leaderboard_data.json")
+        firebase_upload(sorted_user_scores)
+
+
     driver.quit()
